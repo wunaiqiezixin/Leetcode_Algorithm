8c20a6cce9fda79e4469f7fc54657ebf08dacc75
wunaiqiezixin <1947685764@qq.com>
2025-12-25 22:25:39 +0800

    1.Two Sum

--- agorithm.c
+++ agorithm.c
@@ -1,1 +1,27 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int i = 0;i < numsSize - 1;i++)
+    {
+        for (int j = i+1;j < numsSize;j++)
+        {   
+            if (nums[i] + nums[j] == target)
+            {
+               int* result = (int*)malloc(sizeof(int)*2);
+               if (result == NULL)
+               {
+                *returnSize = 0;
+                return NULL;
+               }
+               result[0] = i;
+               result[1] = j;
+               *returnSize = 2;
+               return result;
 
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
+}
